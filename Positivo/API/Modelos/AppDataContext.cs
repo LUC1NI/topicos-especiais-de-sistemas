23c7774f54af7814071ddc0abc9bbae1e84164c9
JOAO VITOR LUCINI <joao.lucini@cs.up.edu.br>
2025-03-24 21:38:12 -0300

    Configuração do EntityFramework

---
+++ Positivo/API/Modelos/AppDataContext.cs
@@ -0,0 +1,13 @@
+using Namespace;
+using Microsoft.EntityFrameworkCore;
+
+public class AppDataContext : DbContext
+{
+    public DbSet<Curso> Cursos { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlite("Data Source=app.db");
+    }
+    
+}
--- Positivo/API/Modelos/Curso.cs
+++ Positivo/API/Modelos/Curso.cs
@@ -4,8 +4,4 @@ public class Curso
     public int Id { get; set; }
     public string Name { get; set; }
 
-    public Curso(int id, string name) {
-        Id = id;
-        Name = name;
-    }
 }
